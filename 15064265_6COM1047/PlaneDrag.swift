b5d587891a7bffd2c29ef5f10d8a3c63ad23a293
Azhar Ahmed <azhariphone@hotmail.com>
2019-01-07 01:37:31 +0000

    1. Create a roadmap background with motion effects.

---
+++ 15064265_6COM1047/PlaneDrag.swift
@@ -0,0 +1,20 @@
+//
+//  PlaneDrag.swift
+//  
+//
+//  Created by Azhar Ahmed on 07/01/2019.
+//
+
+import UIKit
+
+class PlaneDrag: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- 15064265_6COM1047/ViewController.swift
+++ 15064265_6COM1047/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var roadImage: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var imageArray: [UIImage]!
+        
+        imageArray = [UIImage(named: "road1.png")!,
+                      UIImage(named: "road2.png")!,
+                      UIImage(named: "road3.png")!,
+                      UIImage(named: "road4.png")!,
+                      UIImage(named: "road5.png")!,
+                      UIImage(named: "road6.png")!,
+                      UIImage(named: "road7.png")!,
+                      UIImage(named: "road8.png")!,
+                      UIImage(named: "road9.png")!,
+                      UIImage(named: "road10.png")!,
+                      UIImage(named: "road11.png")!,
+                      UIImage(named: "road12.png")!,
+                      UIImage(named: "road13.png")!,
+                      UIImage(named: "road14.png")!,
+                      UIImage(named: "road15.png")!,
+                      UIImage(named: "road16.png")!,
+                      UIImage(named: "road17.png")!,
+                      UIImage(named: "road18.png")!,
+                      UIImage(named: "road19.png")!]
+        
+        roadImage.image = UIImage.animatedImage(with: imageArray, duration: 0.5)
+
+        
+        
+        
     }
 
+   
+    
+    
 
 }
 
